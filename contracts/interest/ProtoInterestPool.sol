aa500408fcfc43a43f63dfe406949bd07d3a78b4
wandbuild <wandbuild@outlook.com>
2023-09-05 11:18:39 +0800

    Add proto interest pool

---
+++ contracts/interest/ProtoInterestPool.sol
@@ -0,0 +1,116 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.18;
+
+import "hardhat/console.sol";
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/Math.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract ProtoInterestPool is Ownable, ReentrancyGuard {
+  using SafeMath for uint256;
+  using SafeERC20 for IERC20;
+
+  /* ========== STATE VARIABLES ========== */
+
+  IERC20 public rewardsToken;
+  IERC20 public stakingToken;
+  uint256 public rewardPerToken;
+
+  mapping(address => uint256) public userRewardPerTokenPaid;
+  mapping(address => uint256) public userRewards;
+
+  uint256 internal _totalSupply;
+  mapping(address => uint256) private _balances;
+
+  /* ========== CONSTRUCTOR ========== */
+
+  constructor(
+    address _rewardsToken,
+    address _stakingToken
+  ) Ownable() {
+    rewardsToken = IERC20(_rewardsToken);
+    stakingToken = IERC20(_stakingToken);
+  }
+
+  /* ========== VIEWS ========== */
+
+  function totalSupply() external view returns (uint256) {
+    return _totalSupply;
+  }
+
+  function balanceOf(address account) external view returns (uint256) {
+    return _balances[account];
+  }
+
+  function earned(address account) public view returns (uint256) {
+    return _balances[account].mul(rewardPerToken.sub(userRewardPerTokenPaid[account])).div(1e18).add(userRewards[account]);
+  }
+
+  /* ========== MUTATIVE FUNCTIONS ========== */
+
+  function stake(uint256 amount) external virtual payable nonReentrant updateReward(msg.sender) {
+    require(amount > 0, "Cannot stake 0");
+    _totalSupply = _totalSupply.add(amount);
+    _balances[msg.sender] = _balances[msg.sender].add(amount);
+    // console.log('stake, msg.sender balance: %s', stakingToken.balanceOf(msg.sender));
+    stakingToken.safeTransferFrom(msg.sender, address(this), amount);
+    emit Staked(msg.sender, amount);
+  }
+
+  function withdraw(uint256 amount) public virtual nonReentrant updateReward(msg.sender) {
+    require(amount > 0, "Cannot withdraw 0");
+    _totalSupply = _totalSupply.sub(amount);
+    _balances[msg.sender] = _balances[msg.sender].sub(amount);
+    stakingToken.safeTransfer(msg.sender, amount);
+    emit Withdrawn(msg.sender, amount);
+  }
+
+  function getReward() public nonReentrant updateReward(msg.sender) {
+    uint256 userReward = userRewards[msg.sender];
+    if (userReward > 0) {
+      userRewards[msg.sender] = 0;
+      rewardsToken.safeTransfer(msg.sender, userReward);
+      emit RewardPaid(msg.sender, userReward);
+    }
+  }
+
+  function exit() external {
+    withdraw(_balances[msg.sender]);
+    getReward();
+  }
+
+  /* ========== RESTRICTED FUNCTIONS ========== */
+
+  function addRewards(uint256 rewardsAmount) external updateReward(address(0)) onlyOwner {
+    require(rewardsAmount > 0, "Too small rewards amount");
+
+    rewardsToken.safeTransferFrom(msg.sender, address(this), rewardsAmount);
+
+    if (_totalSupply > 0) {
+      rewardPerToken = rewardPerToken.add(rewardsAmount.mul(1e18).div(_totalSupply));
+    }
+
+    emit RewardAdded(rewardsAmount);
+  }
+
+  /* ========== MODIFIERS ========== */
+
+  modifier updateReward(address account) {
+    if (account != address(0)) {
+      userRewards[account] = earned(account);
+      userRewardPerTokenPaid[account] = rewardPerToken;
+    }
+    _;
+  }
+
+  /* ========== EVENTS ========== */
+
+  event RewardAdded(uint256 reward);
+  event Staked(address indexed user, uint256 amount);
+  event Withdrawn(address indexed user, uint256 amount);
+  event RewardPaid(address indexed user, uint256 reward);
+  event AdminRewardWithdrawn(address indexed to, uint256 amount);
+}
---
+++ test/5-proto-interest-pool.ts
@@ -0,0 +1,97 @@
+import _ from 'lodash';
+import { ethers } from 'hardhat';
+import { expect } from 'chai';
+import { time } from "@nomicfoundation/hardhat-network-helpers";
+import { anyValue } from '@nomicfoundation/hardhat-chai-matchers/withArgs';
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
+import { ONE_DAY_IN_SECS, deployContractsFixture, expectBigNumberEquals } from './utils';
+import {
+  ERC20Mock__factory,
+  ProtoInterestPool__factory,
+} from '../typechain';
+
+const { provider } = ethers;
+
+describe('Proto Interest Pool', () => {
+
+  it('Proto Interest Pool Works', async () => {
+
+    const { Alice, Bob, Caro } = await loadFixture(deployContractsFixture);
+
+    const ERC20MockFactory = await ethers.getContractFactory('ERC20Mock');
+    const USB = await ERC20MockFactory.deploy("USB Token", "USB");
+    const usb = ERC20Mock__factory.connect(USB.address, provider);
+
+    const ETHx = await ERC20MockFactory.deploy("ETHx Token", "ETHx");
+    const ethx = ERC20Mock__factory.connect(ETHx.address, provider);
+
+    const ProtoInterestPool = await ethers.getContractFactory('ProtoInterestPool');
+    const ProtoInterestPoolContract = await ProtoInterestPool.deploy(ethx.address, usb.address);
+    const protoInterestPool = ProtoInterestPool__factory.connect(ProtoInterestPoolContract.address, provider);
+
+    const genesisTime = (await time.latest()) + ONE_DAY_IN_SECS;
+    await expect(usb.connect(Alice).mint(Bob.address, ethers.utils.parseUnits('10000', await usb.decimals()))).not.to.be.reverted;
+    await expect(usb.connect(Alice).mint(Caro.address, ethers.utils.parseUnits('10000'))).not.to.be.reverted;
+
+    // Bob stakes 800 $USB, and Caro stakes 200 $USB
+    let bobStakeAmount = ethers.utils.parseUnits('800');
+    let caroStakeAmount = ethers.utils.parseUnits('200');
+    await expect(usb.connect(Bob).approve(protoInterestPool.address, bobStakeAmount)).not.to.be.reverted;
+    await expect(protoInterestPool.connect(Bob).stake(bobStakeAmount)).not.to.be.reverted;
+    await expect(usb.connect(Caro).approve(protoInterestPool.address, caroStakeAmount)).not.to.be.reverted;
+    await expect(protoInterestPool.connect(Caro).stake(caroStakeAmount)).not.to.be.reverted;
+    expect(await protoInterestPool.totalSupply()).to.equal(bobStakeAmount.add(caroStakeAmount));
+
+    // Deposit 10000 $USB as reward
+    await time.increaseTo(genesisTime);
+    const totalReward = ethers.utils.parseUnits('10000');
+    await expect(ethx.connect(Alice).mint(Alice.address, totalReward)).not.to.be.reverted;
+    await expect(ethx.connect(Alice).approve(protoInterestPool.address, totalReward)).not.to.be.reverted;
+    await expect(protoInterestPool.connect(Alice).addRewards(totalReward))
+      .to.emit(protoInterestPool, 'RewardAdded').withArgs(totalReward);
+
+    // Bob should immediately get 4/5 rewards, and Caro should get 1/5 rewards
+    expectBigNumberEquals(totalReward.mul(4).div(5), await protoInterestPool.earned(Bob.address));
+    expectBigNumberEquals(totalReward.mul(1).div(5), await protoInterestPool.earned(Caro.address));
+
+    // Bob claim rewards
+    // console.log('Bob earned', ethers.utils.formatUnits((await protoInterestPool.earned(Bob.address)).toString(), 18));
+    await expect(protoInterestPool.connect(Bob).getReward())
+      .to.emit(protoInterestPool, 'RewardPaid').withArgs(Bob.address, anyValue);
+    
+    // Add another round of reward
+    await time.increaseTo(genesisTime + ONE_DAY_IN_SECS * 5);
+    const round2Reward = ethers.utils.parseUnits('20000');
+    await expect(ethx.connect(Alice).mint(Alice.address, round2Reward)).not.to.be.reverted;
+    await expect(ethx.connect(Alice).approve(protoInterestPool.address, round2Reward)).not.to.be.reverted;
+    await expect(protoInterestPool.connect(Alice).addRewards(round2Reward))
+      .to.emit(protoInterestPool, 'RewardAdded').withArgs(round2Reward);
+
+    // Bob should get 4/5 rewards, and Caro should get 1/5 rewards
+    expectBigNumberEquals(round2Reward.mul(4).div(5), await protoInterestPool.earned(Bob.address));
+    expectBigNumberEquals(totalReward.mul(1).div(5).add(round2Reward.mul(1).div(5)), await protoInterestPool.earned(Caro.address));
+
+    // Bob withdraw 600 stakes. Going forward, Bob and Caro should get 1/2 rewards respectively
+    await expect(protoInterestPool.connect(Bob).withdraw(ethers.utils.parseUnits('600'))).not.to.be.reverted;
+
+    // Fast-forward to Day 9. Add new reward
+    await time.increaseTo(genesisTime + ONE_DAY_IN_SECS * 9);
+    const round3Reward = ethers.utils.parseUnits('30000');
+    await expect(ethx.connect(Alice).mint(Alice.address, round3Reward)).not.to.be.reverted;
+    await expect(ethx.connect(Alice).approve(protoInterestPool.address, round3Reward)).not.to.be.reverted;
+    await expect(protoInterestPool.connect(Alice).addRewards(round3Reward)).not.to.be.reverted;
+
+    // Fast-forward to Day 10. Add new reward
+    await time.increaseTo(genesisTime + ONE_DAY_IN_SECS * 10);
+    const round4Reward = ethers.utils.parseUnits('33333');
+    await expect(ethx.connect(Alice).mint(Alice.address, round4Reward)).not.to.be.reverted;
+    await expect(ethx.connect(Alice).approve(protoInterestPool.address, round4Reward)).not.to.be.reverted;
+    await expect(protoInterestPool.connect(Alice).addRewards(round4Reward)).not.to.be.reverted;
+
+    // Check Bob and Caro's rewards
+    expectBigNumberEquals(round2Reward.mul(4).div(5).add(round3Reward.mul(1).div(2).add(round4Reward.mul(1).div(2))), await protoInterestPool.earned(Bob.address));
+    expectBigNumberEquals(totalReward.mul(1).div(5).add(round2Reward.mul(1).div(5)).add(round3Reward.mul(1).div(2).add(round4Reward.mul(1).div(2))), await protoInterestPool.earned(Caro.address));
+
+  });
+
+});
